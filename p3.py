c8531e9c8c486721ed1e36e35ab4fc1bae8c4fea
Guilherme Mello <guilhermecfmello@gmail.com>
2020-08-16 18:18:56 -0300

    Falta fazer a descida do gradiente

--- p3.py
+++ p3.py
@@ -20,30 +20,50 @@ def getDatabase(dbName):
         return inputs, outputs
     return []
 
-def calcOutput(e):
-    sum = e.dot(weights)
-    return stepFunction(sum)
+def calcOutput(inputs, weights, bias):
+    sum = inputs[0] * weights[0] + inputs[1] * weights[1] + bias
+    return np.tanh(sum)
+
+def calcWeights(inputs, weights, bias, lr, error):
+    for i in range(len(weights)):
+        # weights[i] = weights[i] + (lr * inputs[i] * error) # old
+        weights[i] = weights[i] - 1/len(inputs)*lr
+    bias = bias - 1/len(inputs)*lr 
+    return weights, bias
 
 
-def trainning(ages = 0, outputs):
+# quadractic error
+def calcError(y, yCalc):
+    error = (yCalc - y)**2
+
+
+def gradient():
+    
+def trainning(inputs, outputs, ages = 0, lr = 0.1):
+    # Defining random weights
+    weights = rd.uniform(-1,1,2)
+    bias = rd.uniform(-1,1)
+
     for a in range(ages):
         for i in range(len(outputs)):
-                
-            outputCalc = calcOutput(np.asarray(inputs[i]))
-            error = outputs[i] - outputCalc
-            totalError += error
-            for j in range(len(weights)):
-                weights[j] = weights[j] + (learningRate * inputs[i][j] * error)
-                print("weights refreshed: " + str(weights[j]))
+            # print(inputs[i][1])
+            # exit()
+            # calculating sum with bias
+            y = calcOutput(inputs[i], weights, bias)
+            
+            error = calcError(outputs[i], y)
+        
+            
+            # print(y)
+            weights, bias = calcWeights(inputs[i], weights, bias, lr, error)
+            # print(weights, bias)
+            
+
+
 
 
 inputs, outputs = getDatabase('perceptron-database.csv')
-# Defining random weights
-weights = rd.uniform(-1,1,[1,3])
+ages = 1
 learningRate = 0.1
-
-
-
-
-trainning(1)
+trainning(inputs, outputs, ages, learningRate)
 
